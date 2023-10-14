824cf0a1cd5a307e06e8f1ae633c447292c1e8fb
Fredrik Nordström <livog@live.com>
2023-10-14 22:13:40 +0200

    fix: req dont exist in validate function

--- src/utilities/willPathConflict.ts
+++ src/utilities/willPathConflict.ts
@@ -1,23 +1,20 @@
 const willPathConflict = async ({
-  req,
+  payload,
   path,
   currentDocId,
   collectionsToCheck = [],
 }: {
-  req?: any
+  payload?: any
   path: string
   currentDocId: string
   collectionsToCheck: string[]
 }): Promise<Boolean> => {
-  if (!req) return false
-  const { payload, locale } = req
   if (!payload || collectionsToCheck.length === 0) return false // Just make it pass the check.
 
   const foundDocs = await Promise.all(
     // This is for Mongo DB support. This takes ~140ms where union & selct drizzle query takes ~40ms, but it's not a big deal.
     collectionsToCheck?.map(collectionItem => {
       return payload.find({
-        req,
         collection: collectionItem,
         where: {
           path: {
@@ -27,7 +24,6 @@ const willPathConflict = async ({
             not_equals: currentDocId,
           },
         },
-        locale,
         depth: 0,
       })
     }),
