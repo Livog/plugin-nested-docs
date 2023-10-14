f89c82e1ebadee28a62855cc02c698a9e1a27763
Fredrik Nordström <livog@live.com>
2023-10-14 21:10:02 +0200

    chore: switch to deepmerge (better readability)

--- src/fields/breadcrumbs.ts
+++ src/fields/breadcrumbs.ts
@@ -1,50 +1,49 @@
+import deepMerge from 'deepmerge'
 import type { ArrayField } from 'payload/dist/fields/config/types'
 import type { Field } from 'payload/types'
 
-const createBreadcrumbsField = (
-  relationTo: string,
-  overrides: Partial<ArrayField> = {},
-): Field => ({
-  name: 'breadcrumbs',
-  type: 'array',
-  localized: true,
-  fields: [
-    {
-      name: 'doc',
-      type: 'relationship',
-      relationTo,
-      maxDepth: 0,
-      admin: {
-        disabled: true,
+const createBreadcrumbsField = (relationTo: string, overrides: Partial<ArrayField> = {}): Field => {
+  const defaultField: Partial<ArrayField> = {
+    name: 'breadcrumbs',
+    type: 'array',
+    localized: true,
+    fields: [
+      {
+        name: 'doc',
+        type: 'relationship',
+        relationTo,
+        maxDepth: 0,
+        admin: {
+          disabled: true,
+        },
       },
-    },
-    {
-      type: 'row',
-      fields: [
-        {
-          name: 'url',
-          label: 'URL',
-          type: 'text',
-          admin: {
-            width: '50%',
+      {
+        type: 'row',
+        fields: [
+          {
+            name: 'url',
+            label: 'URL',
+            type: 'text',
+            admin: {
+              width: '50%',
+            },
           },
-        },
-        {
-          name: 'label',
-          type: 'text',
-          admin: {
-            width: '50%',
+          {
+            name: 'label',
+            type: 'text',
+            admin: {
+              width: '50%',
+            },
           },
-        },
-      ],
+        ],
+      },
+    ],
+    admin: {
+      readOnly: true,
     },
-    ...(overrides?.fields || []),
-  ],
-  admin: {
-    readOnly: true,
-    ...(overrides?.admin || {}),
-  },
-  ...(overrides || {}),
-})
+  }
+
+  return deepMerge(defaultField, overrides)
+}
 
 export default createBreadcrumbsField
--- src/fields/parent.ts
+++ src/fields/parent.ts
@@ -1,26 +1,25 @@
+import deepMerge from 'deepmerge'
 import type { RelationshipField } from 'payload/dist/fields/config/types'
 
 const createParentField = (
   relationTo: string,
-  overrides?: Partial<
-    RelationshipField & {
-      hasMany: false
-    }
-  >,
-): RelationshipField => ({
-  name: 'parent',
-  relationTo,
-  type: 'relationship',
-  maxDepth: 1,
-  filterOptions: ({ id }) => ({
-    id: { not_equals: id },
-    'breadcrumbs.doc': { not_in: [id] },
-  }),
-  admin: {
-    position: 'sidebar',
-    ...(overrides?.admin || {}),
-  },
-  ...(overrides || {}),
-})
+  overrides?: Partial<RelationshipField>,
+): RelationshipField => {
+  const defaultField: Partial<RelationshipField> = {
+    name: 'parent',
+    relationTo,
+    type: 'relationship',
+    maxDepth: 1,
+    filterOptions: ({ id }) => ({
+      id: { not_equals: id },
+      'breadcrumbs.doc': { not_in: [id] },
+    }),
+    admin: {
+      position: 'sidebar',
+    },
+  }
+
+  return deepMerge(defaultField, overrides || {})
+}
 
 export default createParentField
